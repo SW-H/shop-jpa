5d8ae6e19404ece4c5ff71473f6f9609d8ca05ba
SW-H <hsw08@naver.com>
2023-08-09 14:43:01 +0900

    feat : 엔티티 설계 [주의사항] - @XToOne(OneToOne, ManyToOne)관계는 기본이 즉시로딩(Eager)이므로 직접 지연로딩(lazy)으로 설정해야 함   ex) @OneToOne(fetch = FetchType.LAZY)
    - 컬렉션은 필드에서 초기화
      - 하이버네이트가 엔티티를 영속화 할 때 한번 더 감싸서하이버네이트가 제공하는 내장 컬레션으로 변경한다. 그런데 필드에서 바로 초기화 하지 않고 다른 메서드에서 컬렉션을 잘못 생성하면 하이버네이트 내부 매커니즘에 문제가 발생할 수 있음
    
      ex)
      `private List<OrderItem> orderItems = new ArrayList<>();`

---
+++ src/main/java/com/seungwon/shop/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package com.seungwon.shop.domain;
+
+public enum DeliveryStatus {
+	READY, COMPLETED
+}
---
+++ src/main/java/com/seungwon/shop/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.seungwon.shop.domain;
+
+public enum OrderStatus {
+	ORDER, CANCEL
+}
